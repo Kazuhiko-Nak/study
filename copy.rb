086d50480cda4f8e38377568e19b05c358aea11c
Kazuhiko Nakashima <kazuhiko.nakashima@gmail.com>
2026-02-13 15:40:31 +0900

    add copy.rn

---
+++ copy.rb
@@ -0,0 +1,12 @@
+#標準入力から出力させるだけ。
+
+# encoding: UTF-8
+$stdout.set_encoding('UTF-8')
+$stdin.set_encoding('UTF-8')
+
+# $stdout.write($stdin.read) 最も簡単なCOPY
+
+while (c = $stdin.getc) != nil
+   print c
+end
+    
